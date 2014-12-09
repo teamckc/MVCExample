6dad8289b2617e0147d694af512992e5c5dbf4c6
TranKaho <ka.tran@mrjaffesclass.com>
2014-12-08 22:18:10 -0800

    Update Model.java

--- src/com/mrjaffesclass/apcs/mvc/template/Model.java
+++ src/com/mrjaffesclass/apcs/mvc/template/Model.java
@@ -1,103 +1,83 @@
 package com.mrjaffesclass.apcs.mvc.template;
 
 import com.mrjaffesclass.apcs.messenger.*;
+import java.awt.Color;
+import java.awt.Dimension;
 
 /**
  * The model represents the data that the app uses.
+ *
  * @author Roger Jaffe
  * @version 1.0
  */
 public class Model implements MessageHandler {
 
-  // Messaging system for the MVC
-  private final Messenger mvcMessaging;
+    // Messaging system for the MVC
+    private final Messenger mvcMessaging;
+    private boolean[][] bombMap = new boolean[8][8];
 
-  // Model's data variables
-  private int variable1;
-  private int variable2;
-
-  /**
-   * Model constructor: Create the data representation of the program
-   * @param messages Messaging class instantiated by the Controller for 
-   *   local messages between Model, View, and controller
-   */
-  public Model(Messenger messages) {
-    mvcMessaging = messages;
-  }
-  
-  /**
-   * Initialize the model here and subscribe to any required messages
-   */
-  public void init() {
-    mvcMessaging.subscribe("view:changeButton", this);
-    setVariable1(10);
-    setVariable2(-10);
-  }
-  
-  @Override
-  public void messageHandler(String messageName, Object messagePayload) {
-    if (messagePayload != null) {
-      System.out.println("MSG: received by model: "+messageName+" | "+messagePayload.toString());
-    } else {
-      System.out.println("MSG: received by model: "+messageName+" | No data sent");
+    /**
+     * Model constructor: Create the data representation of the program
+     *
+     * @param messages Messaging class instantiated by the Controller for local
+     * messages between Model, View, and controller
+     */
+    public Model(Messenger messages) {
+        mvcMessaging = messages;
     }
-    MessagePayload payload = (MessagePayload)messagePayload;
-    int field = payload.getField();
-    int direction = payload.getDirection();
+
+    /**
+     * Initialize the model here and subscribe to any required messages
+     */
+    public void init() {
+        mvcMessaging.subscribe("Square:position", this);
+        mvcMessaging.subscribe("GameOver:Reset", this);
+        generateBombMap();
+    }
+
+    private void generateBombMap() {
+
+            for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
+                    bombMap[x][y] = false;
+            }
+        }
+        
+        int count = 0;
+        while (count < 10) {
+            int rand1 = (int) (Math.random() * 8);
+            int rand2 = (int) (Math.random() * 8);
+            if (!bombMap[rand1][rand2]) {
+                bombMap[rand1][rand2] = true;
+                count++;
+            }
+
+        }
     
-    if (direction == Constants.UP) {
-      if (field == 1) {
-        setVariable1(getVariable1()+Constants.FIELD_1_INCREMENT);
-      } else {
-        setVariable2(getVariable2()+Constants.FIELD_2_INCREMENT);
-      }
-    } else {
-      if (field == 1) {
-        setVariable1(getVariable1()-Constants.FIELD_1_INCREMENT);
-      } else {
-        setVariable2(getVariable2()-Constants.FIELD_2_INCREMENT);
-      }      
     }
-  }
 
-  /**
-   * Getter function for variable 1
-   * @return Value of variable1
-   */
-  public int getVariable1() {
-    return variable1;
-  }
+    private void checkBombMap(Dimension dimension) {
+        if (bombMap[(int) dimension.width][(int) dimension.height]) {
+            mvcMessaging.notify("Model:LoseALife");
 
-  /**
-   * Setter function for variable 1
-   * @param v New value of variable1
-   */
-  public void setVariable1(int v) {
-    variable1 = v;
-    // When we set a new value to variable 1 we need to also send a
-    // message to let other modules know that the variable value
-    // was changed
-    mvcMessaging.notify("model:variable1Changed", variable1, true);
-  }
-  
-  /**
-   * Getter function for variable 1
-   * @return Value of variable2
-   */
-  public int getVariable2() {
-    return variable2;
-  }
-  
-  /**
-   * Setter function for variable 2
-   * @param v New value of variable 2
-   */
-  public void setVariable2(int v) {
-    variable2 = v;
-    // When we set a new value to variable 2 we need to also send a
-    // message to let other modules know that the variable value
-    // was changed
-    mvcMessaging.notify("model:variable2Changed", variable2, true);
-  }
+        } else {
+            mvcMessaging.notify("Model:ScoreAPoint");
+
+        }
+    }
+
+    private void resetBoard() {
+        generateBombMap();
+    }
+
+    @Override
+    public void messageHandler(String messageName, Object messagePayload) {
+        switch (messageName) {
+            case "Square:position":
+                checkBombMap((Dimension) messagePayload);
+                break;
+                 case "GameOver:Reset": resetBoard();break;
+        }
+    }
 
 }
